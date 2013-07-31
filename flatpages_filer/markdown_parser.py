be131949c4c663199454f31d40a1652f9a152ef4
Christopher Clarke <cclarke@chrisdev.com>
2013-07-31 08:04:38 -0400

    The signature of the defualt parse method is wrong.
    Does not handle extensions.
    A test needs to be written but lendls will test on test site

--- flatpages_filer/markdown_parser.py
+++ flatpages_filer/markdown_parser.py
@@ -6,18 +6,20 @@ from django.core.exceptions import ObjectDoesNotExist
 img_ref_re = re.compile(IMAGE_REFERENCE_RE)
 reference_re = re.compile(REFERENCE_RE)
 
-def parse(text):
 
-    md = markdown.Markdown(['extra','codehilite'])
+def parse(text, **kwargs):
+
+    extensions = kwargs.pop('extensions', ['extra'])
+    md = markdown.Markdown(extensions)
 
     for iref in re.findall(img_ref_re, text):
         img_id = iref[7]
         alt_txt = iref[0]
         try:
-            fp_img= File.objects.get(pk=img_id)
+            fp_img = File.objects.get(pk=img_id)
             md.references[img_id] = (fp_img.url, alt_txt)
         except ObjectDoesNotExist:
-           pass
+            pass
     for lref in re.findall(reference_re, text):
         a_id = lref[7]
         alt_txt = lref[0]
@@ -27,11 +29,4 @@ def parse(text):
         except ObjectDoesNotExist:
             pass
 
-
     return md.convert(text)
-
-if __name__ == "__main__":
-    #import doctest
-    #doctest.testmod()
-    #print parse('![Alt text 1][1]')
-    print parse('[an example] [id] ')
